8d0899d267482df1dd65274c20e5a716b23ef9dc
Faheem <faheemxxx456@gmail.com>
2025-09-14 12:52:44 +0530

    add dynamic input fields for entries

--- script.js
+++ script.js
@@ -15,4 +15,13 @@ function isInvalidString{
   const regex=/[0-9]-e-[0-9]/;
   return str.match(regex);
 }
-function 
+function addEntry(){
+  const targetInputContainer = document.querySelectorAll(`#${entryDropdown.value}.input-container`);
+  const entryNumber=targetInputContainer.querySelectorAll('input[text="name"]'.length);
+  const HTMLstring=`
+  <label for= "${entryDropdown.value}-${entryNumber}-name>Entry ${entryNumber} Name</label>
+  <input id="${entryDropdown.value}-${entryNumber}-name" type="text" placeholder="name"/>
+  <label for= "${entryDropdown.value}-${entryNumber}-calories>Entry ${entryNumber} Name</label>
+  <input id="${entryDropdown.value}-${entryNumber}-calories" type="number" placeholder="calories" min="0"/>
+  `
+}
